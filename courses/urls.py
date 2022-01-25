48e3b7de335777d9a0c873af7c6a492d90111ab5
ankitoscar <ankitoscar911@gmail.com>
2022-01-25 13:57:31 +0530

    Updated courses app

--- courses/urls.py
+++ courses/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
 
     path('course_request/',views.create_course_request,name='create_course_request'),
+    path('course_details/',views.show_course_details,name='course_details'),
+    path('delete_request/',views.delete_request,name='delete_request'),
+
 
 
 
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
-
 from django.shortcuts import render,redirect
+from .models import *
+from students.models import *
 
 
 # Create your views here.
@@ -15,7 +16,41 @@ def create_course_request(request):
     course_request.requested_student_id = student.objects.get(student_id=request.session['sid'])
     course_request.approval_status = 'Pending'
     course_request.save()
+    notification=notifications()
+    notification.msg='You have requested for enrollment in course '+course.course_name+' by: '+course.course_instructor_id.name
+    notification.s_id=student.objects.get(student_id=request.session['sid'])
+    notification.save()
     # print(list(request.POST.keys()))
+
+
+
 
     return redirect('show_all_courses')
 
+def show_course_details(request):
+    print(request.POST)
+    course_details=courses.objects.get(course_id=request.POST['course_id'])
+
+
+    return render(request,'courses/view_course_details.html',{'crs':course_details})
+
+
+def delete_request(request):
+    print(request.POST)
+    print("Delete request")
+    print(course_requests.objects.get(id=request.POST['request_id']))
+    course_request = course_requests.objects.get(id=request.POST['request_id'])
+    course_requests.objects.get(id=request.POST['request_id']).delete()
+
+    if request.POST['access_type']=='student':
+        notification = notifications()
+        notification.msg = 'You have deleted your requested for enrollment in course ' + course_request.requested_course_id.course_name + ' by: ' + course_request.course_instructor_id.name
+        notification.s_id = student.objects.get(student_id=request.session['sid'])
+        notification.save()
+        return  redirect('/show_requested_courses/')
+    else:
+        notification = notifications()
+        notification.msg = 'Rejected Request!\nYour requested for enrollment in course ' + course_request.requested_course_id.course_name + ' by: ' + course_request.course_instructor_id.name +'has been rejected by the course instructor'
+        notification.s_id = student.objects.get(student_id=request.session['sid'])
+        notification.save()
+        return redirect('teacherrequests')
